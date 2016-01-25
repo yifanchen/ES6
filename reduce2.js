a7e219342041f3eb62eba83dbe62158b4a93a1ca
ychen <ychen@overstock.com>
2016-01-24 19:45:07 -0700

    update

--- reduce2.js
+++ reduce2.js
@@ -10,9 +10,29 @@ var output = fs.readFileSync('data.txt', 'utf8')
     customers[line[0]].push({
       name: line[1],
       price: line[2],
-      quality: line[3]
+      quantity: line[3]
     });
     return customers; 
-  }, {})
+  }, {});
 
-console.log('output:', output);
+console.log('output:', JSON.stringify(output, null, 2)); 
+
+var orders2 = [
+    ["Mark Johansson", "waffle iron", 80, 2],
+    ["Mark Johansson", "blender", 200, 1],
+    ["Mark Johansson", "knife", 10, 4],
+    ["Nikita Smith", "waffle iron", 80, 1],
+    ["Nikita Smith", "knife", 10, 2],
+    ["Nikita Smith", "pot", 20, 3]
+];
+
+var output = orders2.reduce((customers, line) => {
+    customers[line[0]] = customers[line[0]] || [];
+    customers[line[0]].push({
+        name: line[1],
+        price: line[2],
+        quantity: line[3]
+    });
+    return customers;
+}, {});
+console.log("orders2 output:", JSON.stringify(output, null, 2));
