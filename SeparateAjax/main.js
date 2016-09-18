4eea5ddf0d37f06c42cf09dc5658f98ea783142e
Yifan Chen <yifanchn@yahoo.com>
2016-09-17 23:03:07 -0600

    udpate

--- SeparateAjax/main.js
+++ SeparateAjax/main.js
@@ -1,4 +1,5 @@
 // This is an exmaple of separating Ajax out by using Promise()
+// Good to know there is way to seprate the ajax logic out of the function.
 
 function a() {
   let p = new Promise((reslove, reject) => {
